96e8bb0b835617937aafed0123cc491c4f4c751e
hong-il <clroyan01@gmail.com>
2020-02-18 17:02:18 +0900

    Create lifecycle - on init on destroy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PeekABooComponent } from './peek-a-boo.component';
 import { PeekABooParentComponent} from './peek-a-boo-parent.component';
 
+import { SpyParentComponent } from './spy.component';
+import { SpyDirective } from './spy.directive';
+
 @NgModule({
   declarations: [
     AppComponent,
     PeekABooComponent,
-    PeekABooParentComponent
+    PeekABooParentComponent,
+    SpyParentComponent,
+    SpyDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/spy.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+import { LoggerService }  from './logger.service';
+
+@Component({
+  selector: 'spy-parent',
+  templateUrl: './spy.component.html',
+  styles: [
+     '.parent {background: khaki;}',
+     '.heroes {background: LightYellow; padding: 0 8px}'
+  ],
+  providers:  [LoggerService]
+})
+export class SpyParentComponent {
+  newName = 'kim';
+  heroes: string[] = ['hong-il', 'github.com/hong-il'];
+
+  constructor(public logger: LoggerService) {
+  }
+
+  addHero() {
+    if (this.newName.trim()) {
+      this.heroes.push(this.newName.trim());
+      this.newName = '';
+      this.logger.tick();
+    }
+  }
+  
+  removeHero(hero: string) {
+    this.heroes.splice(this.heroes.indexOf(hero), 1);
+    this.logger.tick();
+  }
+
+  reset() {
+    this.logger.log('-- reset --');
+    this.heroes = [];
+    this.logger.tick();
+  }
+}
